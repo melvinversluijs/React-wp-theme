abdef1e5812c5ce6b1922152cc1f76bb5c30f6f7
Melvin Versluijs <melvin.versluijs@outlook.com>
2019-08-23 18:54:57 +0200

    Add eslint configuration & test script.

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,46 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  devtool: "inline-source-map",
-  target: "web",
+  devtool: 'inline-source-map',
+  target: 'web',
   entry: {
-    main: "./frontend/src/main.js"
+    main: './frontend/src/main.js',
   },
   output: {
     path: `${__dirname}/frontend/dist`,
-    publicPath: "/",
-    filename: "main.js"
+    publicPath: '/',
+    filename: 'main.js',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ['babel-loader'],
       },
       {
         test: /\.scss$/,
         exclude: /node_modules/,
         use: [
           MiniCssExtractPlugin.loader,
-          { loader: "css-loader", options: { importLoaders: 1 } },
-          "postcss-loader",
-          "sass-loader"
-        ]
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader',
+          'sass-loader',
+        ],
       },
       {
         test: /\.(png|jpg|jpeg|gif|woff|woff2)$/,
         exclude: /node_modules/,
-        use: ["url-loader"]
-      }
-    ]
+        use: ['url-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss'],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "style.css"
-    })
-  ]
+      filename: 'style.css',
+    }),
+  ],
 };
